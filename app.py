7a4e79c052ff9339ba8f5d0f5800f0084ec93c3e
doca <docafavarato@gmail.com>
2023-01-24 18:12:48 -0300

    added a filter_by_genre function

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from models import retrieve_popular, movie_details_, search_movie, retrieve_similar, movie_trailer
+from models import retrieve_popular, movie_details_, search_movie, retrieve_similar, movie_trailer, retrieve_by_genre
 from flask import Flask, render_template, request
 import tmdbsimple as tmdb
 
@@ -13,7 +13,7 @@ def index():
 def index_post():
     movie_name = request.form.get('movie_name')
     data = search_movie(movie_name)
-    return render_template('movie_search.html', search_data=data)
+    return render_template('movie_search.html', search_data=data, search=movie_name)
 
 @app.route('/<movie_name>')
 def movie_details(movie_name):
@@ -21,6 +21,23 @@ def movie_details(movie_name):
     similar = retrieve_similar(movie_name)
     trailer = movie_trailer(movie_name)
     return render_template('movie_details.html', data=data, similar=similar, trailer=trailer)
+
+@app.route('/gêneros/<genre>', methods=['GET', 'POST'])
+def by_genre(genre):
+    if request.method == 'POST':
+        movie_name = request.form.get('movie_name')
+        data = search_movie(movie_name)
+        return render_template('movie_search.html', search_data=data, search=movie_name)
+
+    elif request.method == 'GET':
+        genre_data = retrieve_by_genre(genre)
+        genre_names = {'28': 'Ação', '12': 'Aventura', '16': 'Animação', '35': 'Comédia', '80': 'Crime', '99': 'Documentário', '18': 'Drama',
+                   '10751': 'Família', '14': 'Fantasia', '27': 'Terror', '36': 'História', '10402': 'Música', '9648': 'Mistério', '10749': 'Romance',
+                   '878': 'Ficção Científica', '10770': 'Cinema TV', '53': 'Thriller', '10752': 'Guerra', '37': 'Faroeste'}
+        return render_template('filtered_genre.html', genre_data=genre_data, genre_name=genre_names[genre])
+
+        
+
 
 @app.errorhandler(500)
 def not_found(e):
--- models.py
+++ models.py
@@ -26,6 +26,30 @@ def retrieve_popular():
 
     return movies
 
+def retrieve_by_genre(genre):
+    urls = [
+        f'https://api.themoviedb.org/3/discover/movie?api_key=937aba2a3907c25e0509540bbe39f3a8&with_genres={genre}&language=pt-br&page={i}' for i in range(40)
+    ]
+
+    session = requests.Session()
+    def get_url(url):
+        response = session.get(url)
+        return response.json()
+
+    movies = {}
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        results = [executor.submit(get_url, url) for url in urls]
+
+        for future in concurrent.futures.as_completed(results):
+            data = future.result()
+            for i in range(19):
+                try:
+                    movies[data['results'][i]['title']] = [data['results'][i]['overview'], data['results'][i]['poster_path'], data['results'][i]['release_date'], data['results'][i]['id']]
+                except:
+                    continue
+
+    return movies
+
 def retrieve_similar(query):
     req = requests.get(f'https://api.themoviedb.org/3/movie/{query}/similar?api_key=937aba2a3907c25e0509540bbe39f3a8&language=pt-br&page=1')
     data = req.json()
@@ -88,5 +112,3 @@ def search_movie(query):
 
     return movies
 
-
-print(movie_trailer(678))

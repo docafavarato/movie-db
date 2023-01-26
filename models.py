97cbc23f5851ef52ba0c7de76e5ee208f198a15e
doca <docafavarato@gmail.com>
2023-01-26 16:37:05 -0300

    created a function that retrieves movies providers for the selected movie

--- models.py
+++ models.py
@@ -12,7 +12,7 @@ def retrieve_popular():
         response = session.get(url)
         return response.json()
 
-    movies = {}
+    movies = dict()
     with concurrent.futures.ThreadPoolExecutor() as executor:
         results = [executor.submit(get_url, url) for url in urls]
 
@@ -40,7 +40,7 @@ def retrieve_by_genre(genre):
         response = session.get(url)
         return response.json()
 
-    movies = {}
+    movies = dict()
     with concurrent.futures.ThreadPoolExecutor() as executor:
         results = [executor.submit(get_url, url) for url in urls]
 
@@ -48,7 +48,11 @@ def retrieve_by_genre(genre):
             data = future.result()
             for i in range(19):
                 try:
-                    movies[data['results'][i]['title']] = [data['results'][i]['overview'], data['results'][i]['poster_path'], data['results'][i]['release_date'], data['results'][i]['id']]
+                    if len(data['results'][i]['title']) > 23:
+                        fixed_title = f'''{data['results'][i]['title'][:20]}..'''
+                    else:
+                        fixed_title = data['results'][i]['title'][:20]
+                    movies[data['results'][i]['title']] = [data['results'][i]['overview'], data['results'][i]['poster_path'], data['results'][i]['release_date'], data['results'][i]['id'], fixed_title]
                 except:
                     continue
 
@@ -57,10 +61,15 @@ def retrieve_by_genre(genre):
 def retrieve_similar(query):
     req = requests.get(f'https://api.themoviedb.org/3/movie/{query}/similar?api_key=937aba2a3907c25e0509540bbe39f3a8&language=pt-br&page=1')
     data = req.json()
-    movies = {}
+    movies = dict()
     for i in range(5):
         try:
-            movies[data['results'][i]['title']] = [data['results'][i]['title'], data['results'][i]['poster_path'], data['results'][i]['id']]
+
+            if len(data['results'][i]['title']) > 23:
+                fixed_title = f'''{data['results'][i]['title'][:20]}..'''
+            else:
+                fixed_title = data['results'][i]['title'][:20]
+            movies[data['results'][i]['title']] = [data['results'][i]['title'], data['results'][i]['poster_path'], data['results'][i]['id'], fixed_title]
         except:
             continue
     return movies
@@ -70,7 +79,7 @@ def movie_details_(query):
     tmdb.API_KEY = '937aba2a3907c25e0509540bbe39f3a8'
     request = tmdb.Movies(query)
     movie = request.info(language='pt-br')
-    movie_info = {}
+    movie_info = dict()
     
     movie_info['title'] = request.title
     movie_info['overview'] = request.overview
@@ -86,7 +95,7 @@ def movie_details_(query):
 def movie_trailer(query):
     req = requests.get(f'https://api.themoviedb.org/3/movie/{query}/videos?api_key=937aba2a3907c25e0509540bbe39f3a8')
     data = req.json()
-    links = []
+    links = list()
     count = 0
     for i in data['results']:
         match data['results'][count]['site']:
@@ -108,10 +117,38 @@ def search_movie(query):
     tmdb.API_KEY = '937aba2a3907c25e0509540bbe39f3a8'
     search = tmdb.Search()
     movie = search.movie(query=query, language='pt-br')
-    movies = {}
+    movies = dict()
     count = 0
     for i in search.results:
-        movies[movie['results'][count]['title']] = [movie['results'][count]['title'], movie['results'][count]['poster_path'], movie['results'][count]['id']]
+        if len(movie['results'][count]['title']) > 23:
+            fixed_title = f'''{movie['results'][count]['title'][:20]}..'''
+        else:
+            fixed_title = movie['results'][count]['title'][:20]
+        movies[movie['results'][count]['title']] = [movie['results'][count]['title'], movie['results'][count]['poster_path'], movie['results'][count]['id'], fixed_title]
         count += 1
 
     return movies
+
+def movie_providers(query):
+    req = requests.get(f'https://api.themoviedb.org/3/movie/{query}/watch/providers?api_key=937aba2a3907c25e0509540bbe39f3a8')
+    data = req.json()
+    movies = dict()
+    for i in data['results']:
+        if 'flatrate' in data['results'][i]:
+            match data['results'][i]['flatrate'][0]['provider_name']:
+                case 'HBO Max':
+                    link = 'https://www.hbomax.com/br/pt'
+                case 'Sky Go':
+                    link = 'https://www.assinandosky.tv.br/'
+                case 'Now TV':
+                    link = 'https://www.clarotvmais.com.br/home-landing'
+                case 'Netflix':
+                    link = 'https://www.netflix.com/br/'
+                case _:
+                    link = '#'
+            movies[data['results'][i]['flatrate'][0]['provider_name']] = [data['results'][i]['flatrate'][0]['logo_path'], link]
+        
+    return movies
+        
+
+print(movie_providers(839738))

499e6d2e2a05377b7c7190b040e96399b4e83621
doca <docafavarato@gmail.com>
2023-01-28 17:38:48 -0300

    fixed some stuff

--- app.py
+++ app.py
@@ -1,42 +1,41 @@
-from models import retrieve_popular, movie_details_, search_movie, retrieve_similar, movie_trailer, retrieve_by_genre, movie_providers
+from models import Movies
 from flask import Flask, render_template, request
-import tmdbsimple as tmdb
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    data = retrieve_popular()
+    data = Movies.retrieve_popular()
     return render_template('index.html', data=data)
 
 @app.route('/', methods=['POST'])
 def index_post():
     movie_name = request.form.get('movie_name')
-    data = search_movie(movie_name)
+    data = Movies.search_movie(movie_name)
     return render_template('movie_search.html', search_data=data, search=movie_name)
 
 @app.route('/<movie_name>', methods=['GET', 'POST'])
 def movie_details(movie_name):
     if request.method == 'POST':
         movie_name = request.form.get('movie_name')
-        data = search_movie(movie_name)
+        data = Movies.search_movie(movie_name)
         return render_template('movie_search.html', search_data=data, search=movie_name)
     elif request.method == 'GET':
-        data = movie_details_(movie_name)
-        similar = retrieve_similar(movie_name)
-        trailer = movie_trailer(movie_name)
-        providers = movie_providers(movie_name)
+        data = Movies.movie_details_(movie_name)
+        similar = Movies.retrieve_similar(movie_name)
+        trailer = Movies.movie_trailer(movie_name)
+        providers = Movies.movie_providers(movie_name)
         return render_template('movie_details.html', data=data, similar=similar, trailer=trailer, providers=providers)
 
 @app.route('/gêneros/<genre>', methods=['GET', 'POST'])
 def by_genre(genre):
     if request.method == 'POST':
         movie_name = request.form.get('movie_name')
-        data = search_movie(movie_name)
+        data = Movies.search_movie(movie_name)
         return render_template('movie_search.html', search_data=data, search=movie_name)
 
     elif request.method == 'GET':
-        genre_data = retrieve_by_genre(genre)
+        genre_data = Movies.retrieve_by_genre(genre)
         genre_names = {'28': 'Ação', '12': 'Aventura', '16': 'Animação', '35': 'Comédia', '80': 'Crime', '99': 'Documentário', '18': 'Drama',
                    '10751': 'Família', '14': 'Fantasia', '27': 'Terror', '36': 'História', '10402': 'Música', '9648': 'Mistério', '10749': 'Romance',
                    '878': 'Ficção Científica', '10770': 'Cinema TV', '53': 'Thriller', '10752': 'Guerra', '37': 'Faroeste'}
@@ -47,5 +46,5 @@ def by_genre(genre):
 def not_found(e):
     return render_template('500.html')
 
-
-app.run()
+if __name__ == '__main__':
+    app.run() 
